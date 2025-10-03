15ae6cf0666cee452034836fef0e1faa59113d6d
IsazaJuan <juanisaza219@gmail.com>
2025-10-03 18:12:36 -0500

    Fase 3: Añadida simulación semanal y diagnóstico financiero

--- 1000634637-JUAN ANDRES ISAZA MORENO/1000634637-JUAN ANDRES ISAZA MORENO/app.js
+++ 1000634637-JUAN ANDRES ISAZA MORENO/1000634637-JUAN ANDRES ISAZA MORENO/app.js
@@ -1,5 +1,5 @@
 
-const nombreUsuario = prompt("Bienvenido por favor, ingresae su nombre:");
+const nombreUsuario = prompt("Bienvenido por favor, ingrese su nombre:");
 
 let saldo = 1000;
 let gastosTotales = 0;
@@ -23,3 +23,47 @@ const registrarGasto = (montoGasto) => {
         console.log("Fondos insuficientes para realizar este gasto.");
     }
 };
+
+const simularSemana = () => {
+    console.log("simulando semana de gasto:");
+    for (let dia = 1; dia <= 7; dia++){
+        console.log(`Día ${dia}:Gastando $50`);
+        registrarGasto(50)
+    } 
+    console.log("Semana simulada.");
+}
+
+function diagnosticoFinanciero() {
+    console.log("DIAGNÓSTICO FINANCIERO");
+    if (saldo >= 1000) {
+        console.log("Buen diagnóstico financiero.");
+}else if (saldo >= 500){
+        console.log("Diagnóstico financiero regular.");
+}else if (saldo >= 100){
+        console.log("Diagnóstico financiero malo.");
+}else {
+        console.log("Diagnóstico financiero crítico.");
+}
+let rangoGastos;
+    if (gastosTotales <=200) {
+        rangoGastos = "bajo";
+    } else if (gastosTotales <=500) {
+        rangoGastos = "medio";
+    } else {
+        rangoGastos = "alto";
+    }
+    switch (rangoGastos) {
+        case "bajo":
+            console.log("Tus gastos totales son bajos.");
+            break;
+        case "medio":
+            console.log("Tus gastos totales son medios.");
+            break;
+        case "alto":
+            console.log("Tus gastos totales son altos.");
+            break;
+        default:
+            console.log("No se pudo determinar el rango de gastos.");
+            break;
+    }
+}
